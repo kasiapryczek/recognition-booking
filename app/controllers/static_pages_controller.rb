13eef1cf2f3201c2ce26c8e151a27b6379d1770a
kasiapryczek <kpryczek@gmail.com>
2016-11-15 21:57:15 +0000

    pierwszy commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+  
+  
+    def thank_you
+      @name = params[:name]
+      @adres = params[:adres]
+      @tel = params[:tel]
+      @email = params[:email]
+      @adreswww = params[:adreswww]
+      @date = params[:date]
+      @set = params[:set]
+      @durtion = params[:duration]
+      @amount = params[:amount]
+      @partyname = params[:partyname]
+      @partylocation = params[:partylocation]
+      @partykind = params[:partykind]
+      @people = params[:people]
+      @sponsors = params[:sponsors]
+      @adres = params[:adres]
+      @message = params[:message]
+      ActionMailer::Base.mail(:from => @email, :to => 'kpryczek@gmail.com', :subject => "a new contact message from #{@name}", :body => @message).deliver_now
+    end
+end
